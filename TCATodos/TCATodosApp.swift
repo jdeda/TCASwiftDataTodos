980039b1969db1006bf377ced2b987a63c1446ca
Jesse Deda <76166399+jdeda@users.noreply.github.com>
2024-01-04 17:48:46 -0500

    Initial Commit

---
+++ TCATodos/TCATodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATodosApp.swift
+//  TCATodos
+//
+//  Created by Jesse Deda on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TCATodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
